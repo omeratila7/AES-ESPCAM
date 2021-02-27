db909081f4945f49278bb0d2ad7dc503b0748413
omeratila7 <56802476+omeratila7@users.noreply.github.com>
2021-02-27 19:56:25 +0300

    Add files via upload

---
+++ HTTPcameraServerAes/HTTPcameraServerAes.ino
@@ -0,0 +1,173 @@
+
+/*********
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp32-cam-video-streaming-web-server-camera-home-assistant/
+
+  IMPORTANT!!!
+   - Select Board "AI Thinker ESP32-CAM"
+   - GPIO 0 must be connected to GND to upload a sketch
+   - After connecting GPIO 0 to GND, press the ESP32-CAM on-board RESET button to put your board in flashing mode
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*********/
+
+
+#include "mbedtls/aes.h"
+#include "ESPAsyncWebServer.h"
+
+#include "esp_camera.h"
+#include <WiFi.h>
+
+#include "soc/soc.h" //disable brownout problems
+#include "soc/rtc_cntl_reg.h"  //disable brownout problems
+
+const char* ssid = "VODAFONE_1540";
+const char* password = "159ad157OK20coal";
+
+AsyncWebServer server(80);
+
+
+#define CAMERA_MODEL_AI_THINKER
+
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+
+bool initCamera() {
+
+  camera_config_t config;
+
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  config.frame_size = FRAMESIZE_SVGA;
+  config.jpeg_quality = 10;
+  config.fb_count = 1;
+
+  esp_err_t result = esp_camera_init(&config);
+
+  if (result != ESP_OK) {
+    return false;
+  }
+
+  return true;
+}
+
+
+void* getCapture() {
+  camera_fb_t * fb = NULL;
+  esp_err_t res = ESP_OK;
+
+
+}
+
+void printHex(uint8_t num) {
+  char hexCar[2];
+  sprintf(hexCar, "%02X", num);
+  Serial.print(hexCar);
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
+  if (!initCamera()) {
+    Serial.printf("Failed to initialize camera...");
+    return;
+  }
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  Serial.println(WiFi.localIP());
+
+  server.on("/picture", HTTP_GET, [](AsyncWebServerRequest * request) {
+    camera_fb_t * fb = NULL;
+    size_t _jpg_buf_len = 0;
+    size_t length = 0;
+    char * key = "abcdefghijklmnop";
+
+    mbedtls_aes_context aes;
+    mbedtls_aes_init( &aes );
+
+
+    fb = esp_camera_fb_get();
+
+    _jpg_buf_len = fb->len;
+
+
+    length = _jpg_buf_len;
+    while (length % 16 != 0) {
+      length++;
+    }
+
+    mbedtls_aes_setkey_enc( &aes, (const unsigned char*) key, strlen(key) * 8 );
+
+    uint8_t arr1[512];
+    uint8_t arr2[512];
+    uint8_t arr3[512];
+
+
+    for (int i = 0; i < length; i += 512) {
+      for (int j = 0; j < 512; j++) {
+        arr1[j] = fb->buf[i + j];
+      }
+      uint8_t iv[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+      int x =   mbedtls_aes_crypt_cbc(&aes, MBEDTLS_AES_ENCRYPT, 512, iv , arr1, arr2);
+      for (int k = 0; k < 512; k++) {
+        fb->buf[i + k] = arr2[k];
+      }
+    }
+    mbedtls_aes_free( &aes );
+
+    Serial.printf("MJPG: %uB\n", length);
+
+    request->send_P(200, "plain/text", (const uint8_t *)fb->buf, length);
+
+    esp_camera_fb_return(fb);
+  });
+
+  server.begin();
+}
+void loop() {
+}
---
+++ HTTPclientAES/HTTPclientAES.ino
@@ -0,0 +1,221 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include "mbedtls/aes.h"
+
+
+#include "esp_http_server.h"
+
+
+const char* ssid = "VODAFONE_1540";
+const char* password = "159ad157OK20coal";
+
+
+#define INPUT_BUFFER_LIMIT (128 + 1)
+
+#define PART_BOUNDARY "123456789000000000000987654321"
+
+const char* serverNameCam = "http://192.168.1.5/picture";
+
+
+static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char* _STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char* _STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
+
+unsigned long previousMillis = 0;
+const long interval = 500;
+
+httpd_handle_t stream_httpd = NULL;
+
+static esp_err_t stream_handler(httpd_req_t *req) {
+  esp_err_t res = ESP_OK;
+  char * part_buf[64];
+
+  char * key = "abcdefghijklmnop";
+  mbedtls_aes_context aes;
+  mbedtls_aes_init( &aes );
+
+
+  res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
+  if (res != ESP_OK) {
+    return res;
+  }
+  while (true) {
+
+    String _jpg_buf = httpGETRequest(serverNameCam);
+    size_t _jpg_buf_len = _jpg_buf.length();
+
+    mbedtls_aes_setkey_enc( &aes, (const unsigned char*) key, strlen(key) * 8 );
+
+    uint8_t arr1[512];
+    uint8_t arr2[512];
+
+    if (res == ESP_OK) {
+      size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
+      res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
+    }
+
+    for (int i = 0; i < _jpg_buf_len; i += 512) {
+      for (int j = 0; j < 512; j++) {
+
+        arr1[j] = _jpg_buf[i + j];
+      }
+      uint8_t iv[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+      mbedtls_aes_crypt_cbc(&aes, MBEDTLS_AES_DECRYPT, 512, iv , arr1, arr2);
+
+      if (res == ESP_OK) {
+        res = httpd_resp_send_chunk(req, (const char *)arr2, 512);
+      }
+    }
+
+
+    mbedtls_aes_free( &aes );
+
+    if (res == ESP_OK) {
+      res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
+    }
+    if (res != ESP_OK) {
+      break;
+    }
+    Serial.printf("MJPG: %uB\n", _jpg_buf_len);
+  }
+  return res;
+}
+
+static esp_err_t hexcodes_handler(httpd_req_t *req) {
+  esp_err_t res = ESP_OK;
+
+  char * key = "abcdefghijklmnop";
+  mbedtls_aes_context aes;
+  mbedtls_aes_init( &aes );
+
+
+  res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
+  if (res != ESP_OK) {
+    return res;
+  }
+
+  httpd_resp_set_type(req, "application/json");
+  httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
+
+  String _jpg_buf = httpGETRequest(serverNameCam);
+  size_t _jpg_buf_len = _jpg_buf.length();
+
+  for (int i = 0; i < _jpg_buf_len; i += 512) {
+    char json_response[1024];
+    char * p = json_response;
+    for (int j = 0; j < 512; j++) {
+      p += sprintf(p, "%02X", _jpg_buf[i + j] );
+    }
+    httpd_resp_send_chunk(req, json_response, strlen(json_response));
+  }
+
+
+
+  mbedtls_aes_setkey_enc( &aes, (const unsigned char*) key, strlen(key) * 8 );
+
+  uint8_t arr1[512];
+  uint8_t arr2[512];
+
+
+  httpd_resp_send_chunk(req, "\n\n", 2);
+
+  char * part_buf[64];
+  size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
+  httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
+
+
+  
+  for (int i = 0; i < _jpg_buf_len; i += 512) {
+    for (int j = 0; j < 512; j++) {
+      arr1[j] = _jpg_buf[i + j];
+    }
+
+    uint8_t iv[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    mbedtls_aes_crypt_cbc(&aes, MBEDTLS_AES_DECRYPT, 512, iv , arr1, arr2);
+
+    res = httpd_resp_send_chunk(req, (const char *)arr2, 512);
+  }
+
+  Serial.println();
+
+  res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
+  mbedtls_aes_free( &aes );
+
+  return res;
+}
+
+void startStreamServer() {
+  httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+  config.server_port = 80;
+
+  httpd_uri_t index_uri = {
+    .uri       = "/",
+    .method    = HTTP_GET,
+    .handler   = stream_handler,
+    .user_ctx  = NULL
+  };
+
+  httpd_uri_t hexcodes_uri = {
+    .uri       = "/hexcodes",
+    .method    = HTTP_GET,
+    .handler   = hexcodes_handler,
+    .user_ctx  = NULL
+  };
+
+  if (httpd_start(&stream_httpd, &config) == ESP_OK) {
+    httpd_register_uri_handler(stream_httpd, &index_uri);
+    httpd_register_uri_handler(stream_httpd, &hexcodes_uri);
+  }
+}
+
+
+
+String httpGETRequest(const char* serverName) {
+  HTTPClient http;
+
+  // Your IP address with path or Domain name with URL path
+  http.begin(serverName);
+
+  // Send HTTP POST request
+  int httpResponseCode = http.GET();
+
+  String payload = "--";
+
+  if (httpResponseCode > 0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    payload = http.getString();
+  }
+  else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  // Free resources
+  http.end();
+
+  return payload;
+}
+
+void printHex(uint8_t num) {
+  char hexCar[2];
+  sprintf(hexCar, "%02X", num);
+  Serial.print(hexCar);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+  startStreamServer();
+}
+
+void loop() {
+}
